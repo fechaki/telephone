d677b07d2d4b7bcae9b47e0b8c47e58aa1e4250a
Rafael Bandeira Rodrigues <rafaband@gmail.com>
2024-12-24 05:51:58 -0300

    feat: client result object

---
+++ src/main/java/br/com/fechaki/telephone/client/data/response/ClientValidationResponse.java
@@ -0,0 +1,15 @@
+package br.com.fechaki.telephone.client.data.response;
+
+public record ClientValidationResponse(
+    boolean valid,
+    String number,
+    String local_format,
+    String international_format,
+    String country_prefix,
+    String country_code,
+    String country_name,
+    String location,
+    String carrier,
+    String line_type
+) {
+}
