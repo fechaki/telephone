6cf33ccf87b6eb0f3bbae439963f451c0721dfce
Rafael Bandeira Rodrigues <rafaband@gmail.com>
2025-02-02 10:46:25 -0300

    chore: name update

--- src/main/java/br/com/fechaki/telephone/v1/mapper/TelephoneEnrichmentMapper.java
+++ src/main/java/br/com/fechaki/telephone/v1/mapper/TelephoneEnrichmentMapper.java
@@ -1,12 +1,12 @@
 package br.com.fechaki.telephone.v1.mapper;
 
-import br.com.fechaki.telephone.client.data.response.ClientValidationResponse;
-import br.com.fechaki.telephone.v1.data.entity.TelephoneValidationEntity;
+import br.com.fechaki.telephone.client.data.response.ClientEnrichmentResponse;
+import br.com.fechaki.telephone.v1.data.entity.TelephoneEnrichmentEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper
-public interface TelephoneValidationMapper {
+public interface TelephoneEnrichmentMapper {
     @Mapping(source = "number", target = "phoneNumber")
     @Mapping(source = "local_format", target = "localFormat")
     @Mapping(source = "international_format", target = "internationalFormat")
@@ -14,5 +14,5 @@ public interface TelephoneValidationMapper {
     @Mapping(source = "country_code", target = "countryCode")
     @Mapping(source = "country_name", target = "countryName")
     @Mapping(source = "line_type", target = "lineType")
-    TelephoneValidationEntity toEntity(ClientValidationResponse response);
+    TelephoneEnrichmentEntity toEntity(ClientEnrichmentResponse response);
 }
